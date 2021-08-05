b0f103b2112097d68e33b462050359ac78942f44
nekinci <niyaziekinci5050@gmail.com>
2021-08-05 23:27:26 +0300

    pipe chain added. processes can run such as ' a | b | c'

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <errno.h>
 
 
 #define READ 0
@@ -42,50 +43,56 @@ char** arg_list(char *line) {
     return arg_list;
 }
 
-void do_processes(char** p_list, int p_count) {
+void do_processes(char **p_list, int p_count){
 
-    int pipe_fd[2];
-    pipe(pipe_fd);
-    pid_t pid = fork();
-    if (pid < 0){
-        perror("Fork failed");
-    } else if (pid > 0){
-        // Parent waits child processes
+    int parent_pid = fork();
+    if (parent_pid > 0) {
         wait(NULL);
-        char data[20];
-        close(pipe_fd[1]);
-        read(pipe_fd[0], data, sizeof(data));
-        close(pipe_fd[0]);
-        printf("in parent: %s\n", data);
-    } else {
-
+    } else if (parent_pid == 0){
         int fd[2];
-        pipe(fd);
-        pid_t c_pid = fork();
-        if (c_pid == 0){
-            close(pipe_fd[WRITE]);
-            close(pipe_fd[READ]);
-            close(fd[READ]);
-            dup2(fd[WRITE], STDOUT_FILENO);
-            close(fd[WRITE]);
-            char **program_args = arg_list(p_list[0]);
-            execv(program_args[0], program_args);
-            perror("Hata");
-        } else if(c_pid > 0) {
-            wait(NULL);
-            char **program_args = arg_list(p_list[1]);
-            dup2(fd[READ], STDIN_FILENO);  // Read pipe ucunu standart input dosyasını gösterecek şekilde yönlendirdik. (kopyaladık.)
-            close(fd[WRITE]); // Yazma pipe ucunu kapattık cünkü kullanmayacagız.
-            close(fd[READ]);
+        pid_t pid;
+        int before_in = STDIN_FILENO;
 
-            // Parent process piping
-            close(pipe_fd[0]);
-            dup2(pipe_fd[1], STDOUT_FILENO);
-            close(pipe_fd[1]);
-            execv(program_args[0], program_args);
-            perror("Hata");
-            exit(0);
+        for (int i = 0; i < p_count; i++){
+            pipe(fd);
+            pid = fork();
+            if (pid < 0){
+                perror("An error occured while trying to fork process");
+                exit(EXIT_FAILURE);
+            } else if (pid > 0){
+                // It's parent process and waits the own child process.
+                close(fd[WRITE]);
+                wait(NULL);
+                before_in = fd[READ];
+            } else {
+                close(fd[READ]);
+                dup2(fd[WRITE], STDOUT_FILENO);
+                close(fd[WRITE]);
+                // # process before input file descriptoru ile stdinputu yer degistirir ve böylece bir önceki processten gelen result
+                // process için bir input niteliği taşır.
+                dup2(before_in, STDIN_FILENO);
+                close(before_in);
+                char **args = arg_list(p_list[i]);
+                execv(args[0], args);
+                exit(0);
+            }
         }
+
+        char c;
+        char *data = malloc(sizeof(c) * BUFSIZ);
+        int i = 0;
+        while (read(before_in, &c, sizeof(char) * 1) > 0){
+            data[i++] = c;
+            if (strlen(data) == BUFSIZ){
+                char *new_str = realloc(data, strlen(data) * sizeof(char) * 2);
+                data = new_str;
+            }
+        }
+        close(before_in);
+        printf("%s", data);
+
+    } else {
+        perror("Process failed");
     }
 
 }
